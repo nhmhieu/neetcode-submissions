646262a8c7d6df18f883705320ed6608fb9d90b8
nhmhieu <nhmhieu0910@gmail.com>
2026-04-22 15:50:04 +0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int> cnt(26,0);
+
+        for(char c : s) cnt[c - 'a'] ++;
+        for(char c : t) cnt[c - 'a'] --;
+
+        for(int x : cnt){
+            if(x != 0) return false;
+        }
+        return true;
+    }
+};
